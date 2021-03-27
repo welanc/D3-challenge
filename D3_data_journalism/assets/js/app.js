ba3772705cfa5fb1e7c26f24a71689715fa650ee
unknown <welan.c.13+gh@gmail.com>
2021-03-28 00:28:23 +1100

    updated app.js, created tooltip function

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -25,33 +25,33 @@ var chartGroup = svg.append("g")
 
 //------------------------------
 // Create array of object containing x-axis labels
-var xAxisLabels = [{
+var xAxisLabels = {
     poverty: "In Poverty (%)",
     age: "Age (Median)",
     income: "Household Income (Median)"
-}];
+};
 
 // Create array of object containing y-axis labels
-var yAxisLabels = [{
+var yAxisLabels = {
     healthcare: "Lacks Healthcare (%)",
     smokes: "Smokes (%)",
     obesity: "Obese (%)"
-}];
+};
 
 console.log(xAxisLabels);
 console.log(yAxisLabels);
 
 //------------------------------
 // Set default x-axis and y-axis values to first objects
-var chosenXAxis = Object.keys(xAxisLabels[0])[0];
-var chosenYAxis = Object.keys(yAxisLabels[0])[0];
+var chosenXAxis = Object.keys(xAxisLabels)[0];
+var chosenYAxis = Object.keys(yAxisLabels)[0];
 
-console.log(chosenXAxis);
-console.log(chosenYAxis);
+console.log(`Starter x axis: ${chosenXAxis}`);
+console.log(`Start y axis: ${chosenYAxis}`);
 
 //------------------------------
 // Function to set the x-axis scale (as a linear scale)
-function xAxis(data, chosenX) {
+function xScale(data, chosenX) {
     var xLinearScale = d3.scaleLinear()
         .domain([d3.min(data, d => d[chosenX]) * 0.9,
         d3.max(data, d => d[chosenX]) * 1.1])
@@ -61,12 +61,34 @@ function xAxis(data, chosenX) {
 };
 
 // Function to set the y-axis scale (as a linear scale)
-function yAxis(data, chosenY) {
+function yScale(data, chosenY) {
     var yLinearScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d[chosenY]) * 1.1])
         .range([height, 0]);
     return yLinearScale;
 };
+
+//------------------------------
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+
+    return xAxis;
+}
+
+function renderAxes(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+
+    yAxis.transition()
+        .duration(1000)
+        .call(leftAxis);
+
+    return yAxis;
+}
 
 //------------------------------
 // function used for updating circles group with a transition to
@@ -105,6 +127,28 @@ function updateToolTip(chosenXAxis, circlesGroup) {
 }
 
 //------------------------------
+// function used for creating and updating tooltip
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function (d) {
+            return (`${d.state}<br>In Poverty: ${d.poverty}<br>Lacks Healthcare: ${d.healthcare}`);
+        });
+
+    // Create tooltip in the chart
+    chartGroup.call(toolTip);
+
+    // Create event listeners to display and hide the tooltip
+    circlesGroup.on("mouseover", function (data) {
+        toolTip.show(data, this);
+    })
+        // onmouseout event
+        .on("mouseout", function (data, index) {
+            toolTip.hide(data);
+        });
+};
+//------------------------------
 // Import Data
 d3.csv("assets/data/data.csv").then(function (stateData) {
 
@@ -121,12 +165,12 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
     });
 
     // Create scale functions
-    var xScale = xAxis(stateData, chosenXAxis);
-    var yScale = yAxis(stateData, chosenYAxis);
+    var xLinScale = xScale(stateData, chosenXAxis);
+    var yLinScale = yScale(stateData, chosenYAxis);
 
     // Create axis functions
-    var bottomAxis = d3.axisBottom(xScale);
-    var leftAxis = d3.axisLeft(yScale);
+    var bottomAxis = d3.axisBottom(xLinScale);
+    var leftAxis = d3.axisLeft(yLinScale);
 
     // Append Axes to the chart
     chartGroup.append("g")
@@ -142,8 +186,8 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
         .enter()
         .append("circle")
         .classed("stateCircle", true)
-        .attr("cx", d => xScale(d[chosenXAxis]))
-        .attr("cy", d => yScale(d[chosenYAxis]))
+        .attr("cx", d => xLinScale(d[chosenXAxis]))
+        .attr("cy", d => yLinScale(d[chosenYAxis]))
         .attr("r", "15");
 
     var circlesText = chartGroup.selectAll("text.stateText")
@@ -151,62 +195,47 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
         .enter()
         .append("text")
         .classed("stateText", true)
-        .attr("x", d => xScale(d[chosenXAxis]))
-        .attr("y", d => yScale(d[chosenYAxis]))
+        .attr("x", d => xLinScale(d[chosenXAxis]))
+        .attr("y", d => yLinScale(d[chosenYAxis]))
         .attr("dy", ".25em")
         .text(function (d) {
             return d.abbr;
         });
-
-    // Step 6: Initialize tool tip
-    var toolTip = d3.tip()
-        .attr("class", "d3-tip")
-        .offset([80, -60])
-        .html(function (d) {
-            return (`${d.state}<br>In Poverty: ${d.poverty}<br>Lacks Healthcare: ${d.healthcare}`);
-        });
-
-    // Step 7: Create tooltip in the chart
-    chartGroup.call(toolTip);
-
-    // Step 8: Create event listeners to display and hide the tooltip
-    circlesGroup.on("mouseover", function (data) {
-        toolTip.show(data, this);
-    })
-        // onmouseout event
-        .on("mouseout", function (data, index) {
-            toolTip.hide(data);
-        });
+    // Create x-axes labels
+    for (i = 0; i < Object.keys(xAxisLabels).length; i++) {
+        var xKeys = Object.keys(xAxisLabels);
+        var xValues = Object.values(xAxisLabels);
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top + 20 + (20 * i)})`)
+            .attr("class", "aText")
+            .attr("value", xKeys[i])
+            .text(xValues[i]);
+    };
 
     // Create y-axes labels
-    yAxisLabels.forEach((d, i) => {
-        Object.entries(d).forEach(([key, value]) => {
-            console.log(key, value);
-            chartGroup.append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 0 - margin.left + 35 + (20 * [i]))
-                .attr("x", 0 - (height / 2))
-                .attr("class", "aText")
-                .attr("value", key)
-                .text(value);
-        });
-    });
-
-    // Create x-axes labels
-    xAxisLabels.forEach((d, i) => {
+    for (i = 0; i < Object.keys(yAxisLabels).length; i++) {
+        var yKeys = Object.keys(yAxisLabels);
+        var yValues = Object.values(yAxisLabels);
         chartGroup.append("text")
-            .attr("transform", `translate(${width / 2}, ${height + margin.top + 20 + (20 * [i])})`)
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 + margin.left - (20 * i) - 135)
+            .attr("x", 0 - (height / 2))
             .attr("class", "aText")
-            .attr("id", d)
-            .text(d);
-    });
+            .attr("value", yKeys[i])
+            .text(yValues[i]);
+    };
+    // Initialize tool tip
+    var toolTip = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
-    var aText = d3.selectAll(".aText");
 
-    aText.on("click", function (data) {
-        console.log(this);
-        this.append("text").classed("aText active", true);
-    });
+
+
+    // var aText = d3.selectAll(".aText");
+
+    // aText.on("click", function (data) {
+    //     console.log(this);
+    //     this.append("text").classed("aText active", true);
+    // });
 
 }).catch(function (error) {
     console.log(error);
